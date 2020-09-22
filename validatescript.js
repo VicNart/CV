9c38d8d5c5f82a19c81a435158f30b569f17bd76
VicNart <nartey571@gmail.com>
2020-09-22 22:57:09 +0000

    master

--- validatescript.js
+++ validatescript.js
@@ -1,12 +1,12 @@
 function valuestorage() {
-    var fname = document.getElementById("fname").Value;
-    var mname = document.getElementById("mname").Value;
-    var lname = document.getElementById("lname").Value;
-    var gend = document.getElementById("gender").Value;
-    var phonenum = document.getElementById("phone").Value;
-    var mail = document.getElementById("email").Value;
-    var addres = document.getElementById("address").Value;
-    var fullname = fname + "" + mname + "" + lname;
+    let fname = document.getElementById("fname").Value;
+    let mname = document.getElementById("mname").Value;
+    let lname = document.getElementById("lname").Value;
+    let gend = document.getElementById("gender").Value;
+    let phonenum = document.getElementById("phone").Value;
+    let mail = document.getElementById("email").Value;
+    let addres = document.getElementById("address").Value;
+    let fullname = fname + "" + mname + "" + lname;
 
     localStorage.setItem("nam", fullname);
     localStorage.setItem("gen", gend);
