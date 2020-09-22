8ac2bd0eeb2ebe9bb5daa3b2bb92ac4d224ae607
VicNart <nartey571@gmail.com>
2020-09-22 16:24:19 +0000

    master

---
+++ validate2.js
@@ -0,0 +1,5 @@
+document.getElementById("name").innerHTML = localStorage.setItem("nam", fullname);
+document.getElementById("gender").innerHTML = localStorage.setItem("gen", gend);
+document.getElementById("phone").innerHTML = localStorage.setItem("pho", phonenum);
+document.getElementById("email").innerHTML = localStorage.setItem("mai", mail);
+document.getElementById("address").innerHTML = localStorage.setItem("addr", addres);
