781b657032c7161404680ac0c8b8ed29a5f8dbb2
VicNart <nartey571@gmail.com>
2020-09-22 15:43:37 +0000

    master

--- validatescript.js
+++ validatescript.js
@@ -1,18 +1,18 @@
  function storage() {
-    var fname = document.getElementById("fname").nodeValue
-    var mname = document.getElementById("mname").nodeValue
-    var lname = document.getElementById("lname").nodeValue
-    var gend = document.getElementById("gender").Value
-    var phonenum = document.getElementById("phone").Value
-    var mail = document.getElementById("email").Value
-    var addres = document.getElementById("address").Value
-    var fullname = fname + "" + mname + "" + lname
+    var fname = document.getElementById("fname").Value;
+    var mname = document.getElementById("mname").Value;
+    var lname = document.getElementById("lname").Value;
+    var gend = document.getElementById("gender").Value;
+    var phonenum = document.getElementById("phone").Value;
+    var mail = document.getElementById("email").Value;
+    var addres = document.getElementById("address").Value;
+    var fullname = fname + "" + mname + "" + lname;
 
-    localStorage.setItem("nam", fullname)
-    localStorage.setItem("gen", gend)
-    localStorage.setItem("pho", phonenum)
-    localStorage.setItem("mai", mail)
-    localStorage.setItem("addr", addres)
+    localStorage.setItem("nam", fullname);
+    localStorage.setItem("gen", gend);
+    localStorage.setItem("pho", phonenum);
+    localStorage.setItem("mai", mail);
+    localStorage.setItem("addr", addres);
 
 }
 
